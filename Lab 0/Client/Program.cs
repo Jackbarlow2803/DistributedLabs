17161a913a2c7c59b7ffd52b21313c23b19ee6c8
JACK BARLOW <556980@hull.ac.uk>
2020-02-25 19:31:28 +0000

    Copy and paste code

--- Lab 0/Client/Program.cs
+++ Lab 0/Client/Program.cs
@@ -5,12 +5,46 @@ namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args
         {
-            using (TcpClient client = new TcpClient("localhost", 5000))
-            {
+            string hostname; int port;
+            Console.Write("Please enter the destination host or IP address: ");
+            hostname = Console.ReadLine();
+            Console.Write("Please enter the port: ");
+            port = int.Parse(Console.ReadLine());
+            Console.Write("Please enter the message: ");
+            String Message = Console.ReadLine();
+            NetworkStream nStream = client.GetStream();
+            nStream.Write(rawData, 0, rawData.Length);
+            Console.Write("Press <ENTER> to quit the client...");
+            Console.ReadLine();
+            //Unsure how to implement
+            //using (TcpClient client = new TcpClient(hostname, port))
+            //{
 
-            }
+            //}
         }
+        public static byte[] Serialize(string Message, int EncryptionKey)
+        {
+
+            int MessageLength;
+            int index = 0;
+            byte[] ascii = System.Text.Encoding.ASCII.GetBytes(Message);
+            MessageLength = ascii.Length + 9;
+            byte[] rawData = new byte[MessageLength];
+            rawData[index++] = 0;
+            rawData[index++] = 0;
+            rawData[index++] = 0;
+            rawData[index++] = 1;
+            int remainingLength = ascii.Length + 3;
+            rawData[index++] = (byte)((remainingLength & 0xff00) >> 8);
+            rawData[index++] = (byte)(remainingLength & 0xff);
+            rawData[index++] = (byte)EncryptionKey;
+            int bodyLength = ascii.Length;
+            rawData[index++] = (byte)((bodyLength & 0xff00) >> 8);
+            rawData[index++] = (byte)(bodyLength & 0xff);
+            Array.Copy(ascii, 0, rawData, index, ascii.Length);
+            return rawData;
+        }
     }
 }
