2fa6fef65dd927531312445cbb21e13740893d8c
JACK BARLOW <556980@hull.ac.uk>
2020-02-25 18:01:57 +0000

    Initial Client/Server set up

--- Lab 0/Client/Program.cs
+++ Lab 0/Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace Client
 {
@@ -6,7 +7,10 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (TcpClient client = new TcpClient("localhost", 5000))
+            {
+
+            }
         }
     }
 }
--- Lab 0/Server/Program.cs
+++ Lab 0/Server/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            TcpListener server = null;
+            int port = 5000;
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            
         }
     }
 }
